20fe76a1850f9905b7fea0f529d2bdc7e3784ca2
aquib116 <rockygaming85400@gmail.com>
2025-03-15 16:27:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,9 +10,9 @@ class Config:
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1002435562762")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
+    SUDO = list(map(int, getenv("SUDO", "5851158054").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
